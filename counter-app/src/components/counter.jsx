30d08aee551a93cde6be2b2050f336e74c5fee3b
Shehan <ggsmnandathilaka@gmail.com>
2021-04-26 19:58:03 +0530

    Passing data to child components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: [],
   };
 
-  // constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  // }
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
 
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags !!!</p>;
@@ -26,41 +41,21 @@ class Counter extends Component {
 
   handleIncrement = (product) => {
     console.log("product-id", product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
+    this.setState({ value: this.state.value - 1 });
   };
-
-  render() {
-    return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.handleIncrement(product)}
-          className="btn btn-secondary btn-sm m-2"
-        >
-          Increment
-        </button>
-        <button
-          onClick={this.handleDecrement}
-          className="btn btn-secondary btn-sm m-2"
-        >
-          Decrement
-        </button>
-      </div>
-    );
-  }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 4 },
+      { id: 3, value: 5 },
+      { id: 4, value: 10 },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter, index) => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
