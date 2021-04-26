696020514d041ff168c4e8e0412248267bceac4a
Shehan <ggsmnandathilaka@gmail.com>
2021-04-26 17:56:44 +0530

    Initial commit

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    return <main className="container">{this.getMovieCountText()}</main>;
+  }
+
+  getMovieContent() {
+    if (this.state.movies && this.state.movies.length === 0)
+      return <p>There are no movies in the database</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+            </tr>
+          </thead>
+          <tbody>{this.getBodyItems()}</tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  getBodyItems() {
+    const { movies } = this.state;
+    if (movies && movies.length > 0) {
+      return (
+        <React.Fragment>
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDeleteMovies(movie._id)}
+                  className="btn btn-danger btn-secondary"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </React.Fragment>
+      );
+    }
+  }
+
+  handleDeleteMovies = (id) => {
+    deleteMovie(id);
+    this.setState({ movies: getMovies() });
+  };
+}
+export default Movies;
