62cc9ebaf8130cc1a5e34a1e5613e7e7a8bdc168
Shehan <ggsmnandathilaka@gmail.com>
2021-04-26 18:05:44 +0530

    Add counter-app

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: [],
+  };
+
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags !!!</p>;
+
+    return (
+      <ul>
+        {" "}
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}{" "}
+      </ul>
+    );
+  }
+
+  handleIncrement = (product) => {
+    console.log("product-id", product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
