85bfd6a3204ca421e0212f5941504c521b444dd5
Shehan <ggsmnandathilaka@gmail.com>
2021-05-01 21:01:18 +0530

    Raising and handling events

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     tags: [],
   };
 
@@ -17,10 +17,10 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.handleDecrement}
-          className="btn btn-secondary btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-sm btn-danger m-2"
         >
-          Decrement
+          Delete
         </button>
       </div>
     );
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,20 +4,26 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 1 },
+      { id: 1, value: 0 },
       { id: 2, value: 4 },
-      { id: 3, value: 5 },
+      { id: 3, value: 0 },
       { id: 4, value: 10 },
     ],
   };
+
+  handleDelete = (id) => {
+    const counters = this.state.counters.filter((counter) => counter.id !== id);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter, index) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
           ></Counter>
         ))}
       </div>
