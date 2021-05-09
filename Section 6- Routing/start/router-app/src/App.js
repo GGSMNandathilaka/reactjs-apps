f77ad96ec2b413f9d1537b710af31d660fb3bb8d
Shehan <ggsmnandathilaka@gmail.com>
2021-05-09 16:59:40 +0530

    Passing props and router params

--- Section 6- Routing/start/router-app/src/App.js
+++ Section 6- Routing/start/router-app/src/App.js
@@ -5,7 +5,6 @@ import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
-import NotFound from "./components/notFound";
 import "./App.css";
 import { Route, Switch } from "react-router";
 
@@ -16,7 +15,11 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products}></Route>
+            <Route path="/products/:id" component={ProductDetails}></Route>
+            <Route
+              path="/products"
+              render={(props) => <Products sortBy="newest" {...props} />}
+            ></Route>
             <Route path="/posts" component={Posts}></Route>
             <Route path="/admin" component={Dashboard}></Route>
             <Route path="" component={Home}></Route>
